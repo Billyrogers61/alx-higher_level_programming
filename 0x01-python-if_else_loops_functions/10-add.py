9b26ada398cdc95f79d5370db0cdb1d53e478774
Billyrogers <rogerskim61@gmail.com>
2021-11-16 15:09:11 -0800

    Add two intergers

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,8 @@
+'''
+   add - Function to add two intergers.
+   @a: variable with the first interger.
+   @b: variable containing the second interger.
+   Return: Sum of first and second interger.
+'''
+def add(a, b):
+    print(a + b)
