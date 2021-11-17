025132a406ce58b13a52a63dbad81562e491aea2
Billyrogers <rogerskim61@gmail.com>
2021-11-16 16:45:17 -0800

    System call

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 import sys
-word = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+word = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 sys.stderr.write(word)
+sys.exit(1)
+
