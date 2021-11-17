b06b5ddb0d0aa344fa9265b3107733c259fb3555
Billyrogers <rogerskim61@gmail.com>
2021-11-17 14:56:18 -0800

    Power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,8 +1,3 @@
-'''
-   pow - Function to compute power arithmetic.
-   @a: variable contains number to be computed.
-   @b: variable contains number to compute.
-   Return: value after of a to the power of b.
-'''
+#!/usr/bin/python3
 def pow(a, b):
-    print(a**b, end ="\n")
+    return(a**b)
