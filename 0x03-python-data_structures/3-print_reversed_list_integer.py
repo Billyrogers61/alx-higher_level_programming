2ea00917afe1c9bc32a656845c45df94e2c52a44
Billyrogers <rogerskim61@gmail.com>
2021-11-21 12:51:53 -0800

    Print elements in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/src/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    count = 0
+    for num in range(len(my_list)):
+        count = count + 1
+        print("{}".format(my_list[-count]))
