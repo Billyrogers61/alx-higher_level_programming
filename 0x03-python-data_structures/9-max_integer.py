2a6e673730e6ee85aae9232c3db14359a53dbeb3
Billyrogers <rogerskim61@gmail.com>
2021-11-22 14:02:33 -0800

    Find largest value in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,5 +5,5 @@ def max_integer(my_list=[]):
         count += 1
         if count == 0:
             return None
-    my_list.sort()
-    return my_list[-1]
+        my_list.sort()
+        return my_list[-1]
