aab4f56d58bdd0d1bb60852299c4f5fd6f07ad56
Billyrogers <rogerskim61@gmail.com>
2021-11-22 13:54:27 -0800

    Print largest integer value in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    big_int = 0
     count = 0
     for num in my_list:
-        count = count + 1
+        count += 1
         if count == 0:
             return None
-        else:
-            if num > big_int:
-                big_int = num
-            else:
-                continue
-    return big_int
+    my_list.sort()
+    return my_list[-1]
