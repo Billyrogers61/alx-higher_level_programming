38ae431870b088943007da3acba4a68decff12ba
Billyrogers <rogerskim61@gmail.com>
2021-11-22 13:18:31 -0800

    Remove character from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for char in my_string:
-        if char == 'C' or char == 'c':
-            continue
-        print(char, end='')
+    return ''.join(c for c in my_string if c not in 'cC')
