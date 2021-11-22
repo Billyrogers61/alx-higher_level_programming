10461853dcee1276e121d457e415cea7fcd84714
Billyrogers <rogerskim61@gmail.com>
2021-11-22 05:54:53 -0800

    Largest integer value in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     big_int = 0
+    count = 0
     for num in my_list:
-        if num > big_int:
-            big_int = num
+        count = count + 1
+        if count == 0:
+            return None
         else:
-            continue
+            if num > big_int:
+                big_int = num
+            else:
+                continue
     return big_int
