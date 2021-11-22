0f19945f311c3d1f5175c166614c0145d5857471
Billyrogers <rogerskim61@gmail.com>
2021-11-22 11:55:15 -0800

    Get an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx >= len(my_list) or idx < 0:
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
         return None
     else:
-        print("Element at index {} is {}".format(idx, my_list[idx]))
+        return my_list[idx]
