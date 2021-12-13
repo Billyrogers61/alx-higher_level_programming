57a528d762591f2614eeeafaf6a9a53e01f7b928
Billyrogers <rogerskim61@gmail.com>
2021-12-13 12:38:04 -0800

    Python Classes and Objects

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """__init_method.
+        Args:
+             width (int): integer width
+             height (int): interger height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width: return width
+        Return: rectangle width
+        """
+        return (self.__width)
+
+    @property
+    def height(self):
+        """height: returns height
+        Returns: rectangle height
+        """
+        return (self.__height)
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            print("width must be an interger")
+        elif value < 0:
+            print("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            print("width must be an interger")
+        elif value < 0:
+            print("height must be >= 0")
+        else:
+            self.__height = value
